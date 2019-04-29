a0230e2505761053751738b72f13be027c547a61
jaroslav20 <jarek.pokropinski@gmail.com>
2019-04-29 22:34:29 +0200

    Fix error on empty token

--- authentication/auth.js
+++ authentication/auth.js
@@ -3,7 +3,7 @@ const config = require("../configuration/config");
 
 const checkToken = (req, res, next) => {
   let token = req.headers["x-access-token"] || req.headers["authorization"]; // Express headers are auto converted to lowercase
-  if (token.startsWith("Bearer ")) {
+  if (token && token.startsWith("Bearer ")) {
     // Remove Bearer from string
     token = token.slice(7, token.length);
   }
