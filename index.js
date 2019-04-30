be27b7c79fbbfd00a8147cd7110ea48949620a2b
jaroslav20 <jarek.pokropinski@gmail.com>
2019-04-30 11:13:12 +0200

    Prepare for heroku deploy

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const api = require("./routes/api");
 const sec = require("./routes/sec");
 
 const app = express();
+const port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use("/api", api);
 app.use("/sec", auth.checkToken, sec);
 
-app.listen(8080);
+app.listen(port);
