3cbf8e73b26c8baaeab0d7f7d7f7195b47093061
jaroslav20 <jarek.pokropinski@gmail.com>
2019-05-06 11:52:25 +0200

    changes

--- routes/api.js
+++ routes/api.js
@@ -2,15 +2,17 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const config = require("../configuration/config");
 const router = express.Router();
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize(`mysql://sql7290405:${process.env.DB_PASSWORD}@sql7.freemysqlhosting.net:3306/sql7290405`);
-const passwordEnc = require('../authentication/passwordEncryptor')
+const Sequelize = require("sequelize");
+// const sequelize = new Sequelize(`mysql://root:root@localhost:3306/wypozyczalnia_bcdzmiana`);
+const sequelize = (process.env.DB_URL) ? new Sequelize(`mysql://sql7290405:${process.env.DB_PASSWORD}@sql7.freemysqlhosting.net:3306/sql7290405`)
+  : new Sequelize(`mysql://root:root@localhost:3306/wypozyczalnia_bcdzmiana`);
+const passwordEnc = require("../authentication/passwordEncryptor");
 
-router.get("/", function(req, res) {
+router.get("/", function (req, res) {
   res.send("GET route on things.");
 });
 
-router.post("/login", function(req, res, next) {
+router.post("/login", function (req, res, next) {
   if (req.body.username === "admin" && req.body.password === "admin") {
     res.json({
       message: jwt.sign({ username: req.body.username }, config.secret, {
@@ -18,30 +20,77 @@ router.post("/login", function(req, res, next) {
       })
     });
   } else {
-    res.status(403).json({
-      message: "message: 'Incorrect username or password"
-    });
+    sequelize
+      .query("SELECT `zwracanie_hasla`(:login)", { replacements: { login: req.body.login } })
+      .then(([results, metadata]) => {
+        if (results.length < 1) {
+          res.status(403).json({
+            message: "Incorrect username or password"
+          });
+        }
+        const hashPass = Object.values(results[0])[0]
+        passwordEnc.comparePassword(req.body.password, hashPass, (err, isMatch) => {
+          if (err) {
+            res.status(500).json({
+              message: "failed to compare passwords"
+            });
+            return;
+          }
+          if (isMatch) {
+            res.json({
+              message: jwt.sign({ username: req.body.username }, config.secret, {
+                expiresIn: "24h" // expires in 24 hours
+              })
+            });
+            return;
+          }
+          res.status(403).json({
+            message: "Incorrect username or password"
+          });
+        })
+      })
+    return
   }
 });
 
-
-router.post("/register", function(req, res, next) {
-  const pass = passwordEnc.cryptPassword(req.body.password)
-  sequelize
-    .query('CALL `Dodaj_Klienta`(:imie, :nazwisko, :dataUrodzenia, :pesel, :login, :haslo)', 
-          {replacements: { imie: req.body.name, nazwisko: req.body.surname, dataUrodzenia: req.body.dateOfBirth, pesel: req.body.pesel, login: req.body.login, haslo: pass,}})
-    .then(v => res.json({
-      message: 'success'
-    }))
-    .catch(e => {
-      console.log(e)
+router.post("/register", function (req, res, next) {
+  passwordEnc.cryptPassword(req.body.password, (err, hash) => {
+    if (err) {
+      console.error(err);
+      pass = null;
       res.status(400).json({
-        message: e.original.sqlMessage
-      })
-    })
+        message: 'Failed to encrypt password'
+      });
+      return;
+    }
+    const pass = hash
+    sequelize
+      .query(
+        "CALL `Dodaj_Klienta`(:imie, :nazwisko, :dataUrodzenia, :pesel, :login, :haslo)",
+        {
+          replacements: {
+            imie: req.body.name,
+            nazwisko: req.body.surname,
+            dataUrodzenia: req.body.dateOfBirth,
+            pesel: req.body.pesel,
+            login: req.body.login,
+            haslo: `${pass}`
+          }
+        }
+      )
+      .then(v =>
+        res.json({
+          message: "success"
+        })
+      )
+      .catch(e => {
+        console.log(e);
+        res.status(400).json({
+          message: e.original.sqlMessage
+        });
+      });
+  });
 });
-
-
 
 //export this router to use in our index.js
 module.exports = router;
--- routes/sec.js
+++ routes/sec.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const Sequelize = require("sequelize");
+const sequelize = (process.env.DB_URL) ? new Sequelize(`mysql://sql7290405:${process.env.DB_PASSWORD}@sql7.freemysqlhosting.net:3306/sql7290405`)
+  : new Sequelize(`mysql://root:root@localhost:3306/wypozyczalnia_bcdzmiana`);
 
 router.get("/", function(req, res) {
+  res.send("GET route on sec.");
+});
+
+router.get("/loans", function (req, res) {
+
   res.send("GET route on sec.");
 });
 
