8ed2737ca562aa0fccd1e33bf214f8ea7d56152c
jaroslav20 <jarek.pokropinski@gmail.com>
2019-05-06 16:57:46 +0200

    Swagger docs

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const bodyParser = require("body-parser");
 const auth = require("./authentication/auth");
 const api = require("./routes/api");
 const sec = require("./routes/sec");
+const swagger = require("./configuration/swaggerConfig")
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+swagger(app)
 
 app.use(bodyParser.json());
 app.use("/api", api);
--- routes/api.js
+++ routes/api.js
@@ -8,10 +8,39 @@ const sequelize = (process.env.DB_URL) ? new Sequelize(`mysql://sql7290405:${pro
   : new Sequelize(`mysql://root:root@localhost:3306/wypozyczalnia_bcdzmiana`);
 const passwordEnc = require("../authentication/passwordEncryptor");
 
+
+/**
+ * @swagger
+ * /api:
+ *    get:
+ *      description: nil
+ */
 router.get("/", function (req, res) {
   res.send("GET route on things.");
 });
 
+/**
+ * @swagger
+ * /api/login:
+ *    post:
+ *      description: Login
+ *      parameters:
+ *      - in: "body"
+ *        name: "User credencials"
+ *        required: true
+ *        schema:
+ *          type: "object"
+ *          properties:
+ *            username:
+ *              type: "string"
+ *            password:
+ *              type: "string"
+ *      responses:
+ *       200:
+ *         description: Login succeded
+ *       403:
+ *          description: "Incorrect credentials"
+ */
 router.post("/login", function (req, res, next) {
   if (req.body.username === "admin" && req.body.password === "admin") {
     res.json({
@@ -32,7 +61,7 @@ router.post("/login", function (req, res, next) {
         passwordEnc.comparePassword(req.body.password, hashPass, (err, isMatch) => {
           if (err) {
             res.status(500).json({
-              message: "failed to compare passwords"
+              message: "Failed to compare passwords"
             });
             return;
           }
@@ -53,6 +82,31 @@ router.post("/login", function (req, res, next) {
   }
 });
 
+/**
+ * @swagger
+ * /api/register:
+ *    post:
+ *      description: Register
+ *    parameters:
+ *    - in: "body"
+ *      name: "User information"
+ *      required: true
+ *      schema:
+ *        type: "object"
+ *        properties:
+ *          name:
+ *            type: "string"
+ *          surname:
+ *            type: "string"
+ *          dateOfBirth:
+ *            type: "string"
+ *          pesel:
+ *            type: "string"
+ *          login:
+ *            type: "string"
+ *          password:
+ *            type: "string"
+ */
 router.post("/register", function (req, res, next) {
   passwordEnc.cryptPassword(req.body.password, (err, hash) => {
     if (err) {
